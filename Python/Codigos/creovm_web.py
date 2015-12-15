c6ca39af55c44c3df1b9976fe77d7516937d5b4b
Juan Arese <juan.p.arese@gmail.com>
2015-12-14 22:20:26 -0300

    modifico archivos configuracion cobbler

--- Python/Codigos/creovm_web.py
+++ Python/Codigos/creovm_web.py
@@ -1,11 +1,12 @@
 __author__ = 'juan'
 
-import os
+import os, time
 from bottle import get, post, request, run # or route
 
 @get('/') #Pagina principal, la cual pide los datos necesarios para crear la VM.
 def creaVM():
-    peticionhtml = open("/home/juan/Proyecto Integrador Desde Cero/Repositorio/Python/HTMLs/Peticion_parametros_CreacionVM.html","r", 0)
+    peticionhtml = open("/home/werner/demo/HTMLs/Peticion_parametros_CreacionVM.html","r", 0)
+    #peticionhtml = open("/home/juan/Proyecto Integrador Desde Cero/Repositorio/Python/HTMLs/Peticion_parametros_CreacionVM.html","r", 0)
     return peticionhtml
 
 @post('/') #toma los parametros ingresados e indica si se creo o no la VM con exito.
@@ -14,7 +15,15 @@ def do_creaVM():
     ramvm = request.forms.get('ramvm')
     discovm = request.forms.get('discovm')
     MAC = request.forms.get('MAC')
-    os.system("/home/juan/Proyecto\ Integrador\ Desde\ Cero/Repositorio/Codigos\ en\ bash/CreacionVM.sh " + namevm + " " + ramvm  + " " + namevm  + " " + discovm + " " + MAC)
+    IPVM = request.forms.get('IPVM')
+    IPCobbler = request.forms.get('IPCobbler')
+    perfil = request.forms.get('perfil')
+    hostname = request.forms.get('hostname')
+    #Una vez obtenidos todos los paramentros desde la interfaz web, debo primero, crear el sistema.
+    os.system("/home/werner/demo/bash/CreaSistema.sh " + IPCobbler + " " + namevm  + " " +  perfil  + " " +  hostname  + " " +  MAC  + " " + IPVM)
+    #Le doy el tiempo necesario para que cree el sistema.
+    time.sleep(3)
+    os.system("/home/werner/demo/bash/CreacionVM.sh " + namevm + " " + ramvm  + " " + namevm  + " " + discovm + " " + MAC)
     return '''La maquina virtual se creo corectamente'''
 
 
